1751b3901d185b15e2cc4ce3772bcaa8ebd2471c
Shawn Tucker <shqtucke@gmail.com>
2024-05-13 20:53:52 -0400

    Initial Commit

---
+++ NewThree/NewThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewThreeApp.swift
+//  NewThree
+//
+//  Created by Shawn Tucker on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct NewThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
